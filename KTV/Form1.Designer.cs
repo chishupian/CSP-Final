4871fcf49aacdd4c3759394fcb70169a8a8e31b2
Sting-Scorpion <wbc2001@126.com>
2021-06-02 11:49:31 +0800

    KTV播放

---
+++ KTV/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace KTV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.PlayPauseBtn = new System.Windows.Forms.Button();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.ReplayBtn = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.SongNameLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(49, 45);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(100, 30);
+            this.AddBtn.TabIndex = 0;
+            this.AddBtn.Text = "添加歌曲";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(202, 45);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(440, 56);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseDown);
+            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(49, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(577, 139);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // PlayPauseBtn
+            // 
+            this.PlayPauseBtn.Location = new System.Drawing.Point(49, 337);
+            this.PlayPauseBtn.Name = "PlayPauseBtn";
+            this.PlayPauseBtn.Size = new System.Drawing.Size(100, 30);
+            this.PlayPauseBtn.TabIndex = 3;
+            this.PlayPauseBtn.Text = "播放/暂停";
+            this.PlayPauseBtn.UseVisualStyleBackColor = true;
+            this.PlayPauseBtn.Click += new System.EventHandler(this.PlayPauseBtn_Click);
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Location = new System.Drawing.Point(164, 337);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(100, 30);
+            this.NextBtn.TabIndex = 3;
+            this.NextBtn.Text = "下一首";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.NextBtn_Click);
+            // 
+            // ReplayBtn
+            // 
+            this.ReplayBtn.Location = new System.Drawing.Point(291, 337);
+            this.ReplayBtn.Name = "ReplayBtn";
+            this.ReplayBtn.Size = new System.Drawing.Size(100, 30);
+            this.ReplayBtn.TabIndex = 3;
+            this.ReplayBtn.Text = "重唱";
+            this.ReplayBtn.UseVisualStyleBackColor = true;
+            this.ReplayBtn.Click += new System.EventHandler(this.ReplayBtn_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(49, 78);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 4;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // SongNameLabel
+            // 
+            this.SongNameLabel.AutoSize = true;
+            this.SongNameLabel.Location = new System.Drawing.Point(213, 24);
+            this.SongNameLabel.Name = "SongNameLabel";
+            this.SongNameLabel.Size = new System.Drawing.Size(55, 15);
+            this.SongNameLabel.TabIndex = 5;
+            this.SongNameLabel.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SongNameLabel);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.ReplayBtn);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.PlayPauseBtn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.AddBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button PlayPauseBtn;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Button ReplayBtn;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label SongNameLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ KTV/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WMPLib;
+
+namespace KTV
+{
+    public partial class Form1 : Form
+    {
+        List<string> pathlist = new List<string>();
+        double max, min;
+        bool paused = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog song = new OpenFileDialog();
+            song.Multiselect = true;
+            song.Title = "请选择音乐文件";
+            song.Filter = "(*.m4a)|*.m4a";
+            //song.ShowDialog();
+            if(song.ShowDialog() == DialogResult.OK)
+            {
+                string[] namelist = song.FileNames;
+                foreach(string name in namelist)
+                {
+                    string p = Path.GetFileNameWithoutExtension(name);
+                    listBox1.Items.Add(p);
+                    pathlist.Add(name);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = pathlist[listBox1.SelectedIndex];
+            SongNameLabel.Text = listBox1.SelectedItem.ToString();
+            paused = false;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            max = axWindowsMediaPlayer1.currentMedia.duration;
+            min = axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            trackBar1.Maximum = (int)(max);
+            trackBar1.Value = (int)(min);
+            if(axWindowsMediaPlayer1.playState == WMPPlayState.wmppsStopped)
+            {
+                int newIndex = listBox1.SelectedIndex + 1;
+                newIndex = newIndex == listBox1.Items.Count ? 0 : newIndex;
+                axWindowsMediaPlayer1.URL = pathlist[newIndex];
+                listBox1.SelectedIndex = newIndex;
+                SongNameLabel.Text = listBox1.SelectedItem.ToString();
+                trackBar1.Value = 0;
+                timer1.Enabled = true;
+            }
+        }
+
+        private void trackBar1_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = false;
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void trackBar1_MouseUp(object sender, MouseEventArgs e)
+        {
+            double targetValue = trackBar1.Value;
+            axWindowsMediaPlayer1.Ctlcontrols.currentPosition = targetValue;
+            if (!paused) axWindowsMediaPlayer1.Ctlcontrols.play();
+
+            timer1.Enabled = true;
+        }
+
+        private void PlayPauseBtn_Click(object sender, EventArgs e)
+        {
+            if (paused)//暂停时
+            {
+                int selectedIndex = listBox1.SelectedIndex;
+                if (selectedIndex < 0)
+                {
+                    selectedIndex = 0;
+                    listBox1.SelectedIndex = selectedIndex;
+                    axWindowsMediaPlayer1.URL = pathlist[selectedIndex];
+                    SongNameLabel.Text = listBox1.SelectedItem.ToString();
+                }
+                else
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                paused = false;
+            }
+            else//正在播放时
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+                paused = true;
+            }
+        }
+
+        private void ReplayBtn_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+            selectedIndex = selectedIndex < 0 ? 0 : selectedIndex;
+            listBox1.SelectedIndex = selectedIndex;
+            axWindowsMediaPlayer1.URL = pathlist[selectedIndex];
+            SongNameLabel.Text = listBox1.SelectedItem.ToString();
+            paused = false;
+        }
+
+        private void NextBtn_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex + 1;
+            selectedIndex = selectedIndex == listBox1.Items.Count ? 0 : selectedIndex;
+            listBox1.SelectedIndex = selectedIndex;
+            axWindowsMediaPlayer1.URL = pathlist[selectedIndex];
+            SongNameLabel.Text = listBox1.SelectedItem.ToString();
+        }
+    }
+}
